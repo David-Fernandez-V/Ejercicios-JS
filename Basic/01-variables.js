17e2949ac2ef1431e3226578f551bfbb79ab8b9d
David-Fernandez-V <david.fernandez.v04@gmail.com>
2025-02-12 13:39:14 -0600

    Variables

---
+++ Basic/01-variables.js
@@ -0,0 +1,9 @@
+// Tipos de declarar variables
+
+var hello1 = "Hello World 3" //Manera original de declarar. Actualmente no se recomienda utilizar. Se puede acceder a esta antes de ser declarada
+let hello2 = "Hello World 2" //Variables con let solo son accesibles dentro de un bloque. No se puede acceder antes de ser declarada
+const hello3 = "hello World 1" //Constante, no se puede modificar (solo si se asigna a una referencia)
+
+console.log(hello1)
+console.log(hello2)
+console.log(hello3)
