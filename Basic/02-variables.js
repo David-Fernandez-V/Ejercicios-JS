ea7783b870c1c1c978ecaf50ea13c85b27711fc5
David-Fernandez-V <david.fernandez.v04@gmail.com>
2025-02-12 20:51:32 -0600

    Datos primitivos

---
+++ Basic/02-variables.js
@@ -0,0 +1,41 @@
+//String
+let nombre = "David Fernández"
+
+//Número
+let estatura = 73.6
+
+//Booleano
+let esPrincipiante = false
+
+//Undefined
+let sinValor
+console.log(sinValor)
+
+//Null
+let valorNulo = null
+
+if (valorNulo){
+    console.log("Valor asignado")
+}else{
+    console.log("Sin valor")
+}
+
+//Symbol
+let simbolo = Symbol("simboloCreado")
+console.log(simbolo)
+
+//Big Int
+
+let entero_grande = BigInt(548693848484685416843984768468138546846468468488888888666661876433333348677876735435455354684684)
+console.log(entero_grande)
+
+
+//Comprobación
+console.log(typeof nombre)
+console.log(typeof estatura)
+console.log(typeof esPrincipiante)
+console.log(typeof sinValor)
+console.log(typeof valorNulo)
+console.log(typeof simbolo)
+console.log(typeof entero_grande)
+
