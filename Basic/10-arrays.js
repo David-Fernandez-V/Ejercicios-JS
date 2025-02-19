caf5051c82be6e47cf305e8c5ae66c7a1d9b21b0
David-Fernandez-V <david.fernandez.v04@gmail.com>
2025-02-19 12:16:29 -0600

    Estructura array

---
+++ Basic/10-arrays.js
@@ -0,0 +1,23 @@
+let myArray = ["David", 25, true]
+console.log(myArray)
+
+//Métodos comunes
+myArray.push(1.75)
+console.log(myArray)
+
+console.log(myArray.pop())
+console.log(myArray)
+
+console.log(myArray.shift())
+console.log(myArray)
+
+myArray.unshift("David","Fernández")
+console.log(myArray)
+
+mySlice = myArray.slice(1,2)
+console.log(mySlice)
+
+mySlice = myArray.splice(1,2)
+console.log(mySlice)
+console.log(myArray)
+
